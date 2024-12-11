7f476c9f787c475b3053c1d6db3927cf11fcf68e
Michael Ploof <michael.ploof@gmail.com>
2024-12-10 19:50:44 -0500

    Fixed circular import

--- src/marketdata/__init__.py
+++ src/marketdata/__init__.py
@@ -1,5 +1,5 @@
-from .client_async import *
 from .client_params import *
 from .client import *
-from .credentials import *
+from .client_async import *
 from .manager import *
+from .credentials import *
